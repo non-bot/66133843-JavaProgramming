47ca4ea1ca64680d5622917946d212e29863be17
Ngo Quang Tien <tienngoquang1711@gmail.com>
2026-03-11 18:43:16 +0700

    ex_ball_1_9

---
+++ ex_ball_1_9/src/ex_ball_1_9/Ball.java
@@ -0,0 +1,47 @@
+package ex_ball_1_9;
+
+public class Ball {
+	float x, y, xDelta, yDelta;
+	int radius;
+	public Ball(float x, float y, int radius, float xDelta, float yDelta) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.xDelta = xDelta;
+		this.yDelta = yDelta;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	public float getY() {
+		return y;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public float getXDelta() {
+		return xDelta;
+	}
+	public float getYDelta() {
+		return yDelta;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public void setXDelta(float xDelta) {
+		this.xDelta = xDelta;
+	}
+	public void setYDelta(float yDelta) {
+		this.yDelta = yDelta;
+	}
+	public void move() {
+		
+	}
+}
