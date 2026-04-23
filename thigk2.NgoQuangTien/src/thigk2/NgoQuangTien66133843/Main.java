302801615cf035828c1d31b5fd0ef2035e2ae588
Ngo Quang Tien <tienngoquang1711@gmail.com>
2026-04-23 07:31:50 +0700

    Lan commit thu 2

--- thigk2.NgoQuangTien/src/thigk2/NgoQuangTien66133843/Main.java
+++ thigk2.NgoQuangTien/src/thigk2/NgoQuangTien66133843/Main.java
@@ -1,5 +1,15 @@
 package thigk2.NgoQuangTien66133843;
 
+import java.util.Scanner;
+//Tao lop scanner roi dung lop do cho nguoi dung nhap thong tin vao
 public class Main {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap chieu cao cua ban");
+		double chieuCao = sc.nextDouble();
+		sc.nextLine();
+		System.out.println("Nhap can nang cua ban");
+		double canNang = sc.nextDouble();
+		sc.nextLine();
+	}
 }
